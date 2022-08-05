079479faa3a65a2530fa64f6089c4df243648271
Lukas Gerhold <lukger1999@gmail.com>
2022-08-05 18:13:33 +0200

    Added google analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,9 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+    <title>Shorty</title>
+</svelte:head>
 <div>
     <slot></slot>
     <p class="fixed bottom-3 left-1/2 -translate-x-1/2 text-gray-500"> (c) 2022 - Lukas Gerhold</p>
